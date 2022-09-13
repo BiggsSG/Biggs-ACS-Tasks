c31905fda3c5836e4634f5bd417fbcdf10b6993f
BiggsSG <Ramezuuu@gmail.com>
2022-09-13 09:23:50 +0100

    Create Options.py

---
+++ Options.py
@@ -0,0 +1,6 @@
+print ("Please input a number from 1-3")
+Choice = int(input())
+while Choice not in [1,2,3]:
+    Choice = int(input("Please input a number from 1-3 "))
+else:
+    print ("You have selected option number " + str(Choice))
