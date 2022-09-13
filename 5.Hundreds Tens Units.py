26c9646efd6e90bc618efb4b01973ebb8dcd8ffe
BiggsSG <Ramezuuu@gmail.com>
2022-09-13 09:55:56 +0100

    Update 5.Hundreds Tens Units.py

--- 5.Hundreds Tens Units.py
+++ 5.Hundreds Tens Units.py
@@ -2,4 +2,4 @@ Number = int(input("Please input a number between 100-999 "))
 Hundreds = Number // 100
 Tens = (Number % 100) // 10
 Units = (Number % 10)
-print (str(Hundreds) + " hundreds " + str(Tens) " tens " + str(Units) + " units"))
+print (str(Hundreds) + " hundreds " + str(Tens) + " tens "  + str(Units) + " units")
