3d19ff7f18d45e323c460715986a4acd51944eaf
BiggsSG <Ramezuuu@gmail.com>
2022-09-14 09:35:16 +0100

    comment

--- 3.Options.py
+++ 3.Options.py
@@ -1,6 +1,7 @@
 print ("Please input a number from 1-3")
 Choice = int(input())
-while Choice not in [1,2,3]:
-    Choice = int(input("Please input a number from 1-3 "))
+while Choice not in [1,2,3]: # validation for num between 1-3
+    Choice = int(input("Please input a number from 1-3 "))  
 else:
-    print ("You have selected option number " + str(Choice))
+    print ("You have selected option number " + str(Choice)) # converted back to string for concatination 
+# end while
