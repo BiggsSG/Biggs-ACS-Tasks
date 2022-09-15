9ca845f3148b037885c25adaab602e6816f4e9ce
BiggsSG <Ramezuuu@gmail.com>
2022-09-15 11:42:00 +0100

    comment

--- 8.Comparison of Two.py
+++ 8.Comparison of Two.py
@@ -1,6 +1,7 @@
-num1 = int(input("Please input a number "))
+num = int(input("Please input a number "))
 num2 = int(input("Please input a second number "))
-if num2 < num1:
-    print (num1, num2)
+if num2 < num: # if statement to check for the larger number
+    print (num, num2)
 else:
-    print (num2, num1)
+    print (num2, num)
+#end if
