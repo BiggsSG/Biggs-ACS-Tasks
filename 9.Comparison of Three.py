934be542ac9578c505e32550f646a8fe56ece3eb
BiggsSG <Ramezuuu@gmail.com>
2022-09-15 12:04:31 +0100

    file extension

---
+++ 9.Comparison of Three.py
@@ -0,0 +1,6 @@
+num = int(input("Please input a number "))
+num2 = int(input("Please input a second number "))
+num3 = int(input("Please input a third number "))
+for i in(sorted([num, num2, num3], reverse = True)):
+    print (i)
+
