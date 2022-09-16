53f365a48661ace4c107a113dd329460bef0d15e
BiggsSG <Ramezuuu@gmail.com>
2022-09-16 12:14:30 +0100

    a

--- 11.Seconds Anyone.py
+++ 11.Seconds Anyone.py
@@ -1,5 +1,5 @@
-hour = int(input("Please input hours "))
+hour = int(input("Please input hours ")) # convert from string to integer to allow for mathmatical operations
 minute = int(input("Please input minutes "))
 second = int(input("Please input seconds "))
-total = 60 * hour * 60 + minute * 60 + second
+total = 60 * hour * 60 + minute * 60 + second # hours multiplied and minutes to convert them to seconds
 print (total) 
---
+++ 12.Factors.py
@@ -0,0 +1,4 @@
+number = int(input("Please enter a number "))
+for i in range(1, number + 1):
+    print (number//i)
+
