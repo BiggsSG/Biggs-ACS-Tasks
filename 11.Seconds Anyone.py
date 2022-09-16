7fb649e9a3749470ffcf60cc7133757167906ede
BiggsSG <Ramezuuu@gmail.com>
2022-09-16 11:57:37 +0100

    j

---
+++ 11.Seconds Anyone.py
@@ -0,0 +1,5 @@
+hour = int(input("Please input hours "))
+minute = int(input("Please input minutes "))
+second = int(input("Please input seconds "))
+total = 60 * hour * 60 + minute * 60 + second
+print (total) 
