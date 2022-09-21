0c92ec2efcec9aed13e71ffa70b68f747c49f351
BiggsSG <Ramezuuu@gmail.com>
2022-09-21 07:27:50 +0100

    Update 12.Factors.py

--- 12.Factors.py
+++ 12.Factors.py
@@ -1,5 +1,5 @@
 number = int(input("Please enter a number "))
 for i in range(1, number + 1):
-    if number%i == 0: 
-        print (number//i, i)
+    if number%i == 0: # checks number is a whole integer
+        print (number//i)
 
