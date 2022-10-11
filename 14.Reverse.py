2bebbe8371d6a2c8c62c2ff176a51f4fba51dd05
BiggsSG <Ramezuuu@gmail.com>
2022-10-11 12:22:14 +0100

    Update 14.Reverse.py

--- 14.Reverse.py
+++ 14.Reverse.py
@@ -1,5 +1,5 @@
-def reverser(x):
- return x[::-1]
+def reverser(x): # function created to reverse text 
+ return x[::-1] # creates a slice which moves back through the string
 
-reversetext = reverser(input("Enter your word to reverse: "))
-print(reversetext)
+reversetext = reverser(input("Enter your word to reverse: ")) # funtion used on input to reverse text
+print(reversetext) # prints text 
