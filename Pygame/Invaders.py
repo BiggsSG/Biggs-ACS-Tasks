d115c6e5307d3d69cdcc2f9188e1731dc8ed43b6
BiggsSG <Ramezuuu@gmail.com>
2022-12-07 09:42:31 +0000

    Update Invaders.py

--- Pygame/Invaders.py
+++ Pygame/Invaders.py
@@ -22,7 +22,7 @@ class Invader(pygame.sprite.Sprite):
         # Set the position of the sprite 
         self.rect = self.image.get_rect() 
         self.rect.x = random.randrange(0, 600) 
-        self.rect.y = random.randrange(0, -50)
+        self.rect.y = random.randrange(-50, 0)
         # Set speed of the sprite 
         self.speed = speed
          #End Procedure #End Class
@@ -33,8 +33,8 @@ class Invader(pygame.sprite.Sprite):
             self.rect.y = 0
 
 ## -- Define the class Player which is a sprite
-class Player(pygame.sprite.Sprite): 
-    # Define the constructor for snow 
+class player(pygame.sprite.Sprite): 
+    # Define the constructor for player 
     def __init__(self, color, width, height): 
         # Call the sprite constructor 
         super().__init__()
@@ -42,16 +42,19 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface([width,height]) 
         self.image.fill(color) 
         # Set the position of the sprite 
-        self.rect = self.rect(300, size[0] - height)
+        self.rect = self.image.get_rect() 
+        self.rect.x = (300) 
+        self.rect.y = (size[0] - height) 
         # Set speed of the sprite 
-        self.speed = 0 
+        self.speed = 0
+        def player_set_speed(val):
+            self.speed = (val)
          #End Procedure #End Class
     # Class update function - runs for each pass through the game loop 
     def update(self): 
         self.rect.y = self.rect.y + self.speed
         if self.rect.y >480:
             self.rect.y = 0
-
 # -- Initialise PyGame
 pygame.init()
 
@@ -70,8 +73,8 @@ all_sprites_group = pygame.sprite.Group()
 clock = pygame.time.Clock() 
 
 # Create the invaders 
-number_of_invaders = 50 
-# we are creating 50 invaders 
+number_of_invaders = 10
+# we are creating 10 invaders 
 for x in range (number_of_invaders): 
     my_invaders = Invader(BLUE, 10, 10, 1) 
     # invaders are blue with size 10 by 10 px 
@@ -81,15 +84,26 @@ for x in range (number_of_invaders):
     # adds it to the group of all Sprites 
 #Next x
 
+# Create the player 
+Player = player(YELLOW, 10, 10)
+all_sprites_group.add (Player) 
+
 ### -- Game Loop 
 while not done: 
-    # -- User input and controls 
+    # -- User inputs here 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             done = True 
-        #End If 
-    #Next event 
+        elif event.type == pygame.KEYDOWN: # - a key is down 
+            if event.key == pygame.K_LEFT: # - if the left key pressed 
+                player.player_set_speed(-3) # speed set to -3 
+            elif event.key == pygame.K_RIGHT: # - if the right key pressed 
+                player.player_set_speed(3) # speed set to 3 
+        elif event.type == pygame.KEYUP: # - a key released 
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
+                player.player_set_speed(0) # speed set to 0
     # -- Game logic goes after this comment
+    # -- when invader hits the player add 5 to score. 
     all_sprites_group.update()
     # -- Screen background is BLACK 
     screen.fill (BLACK)
